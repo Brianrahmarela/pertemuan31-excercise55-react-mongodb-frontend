56db7d927f9120c157fa7fd58fee993fd96f1e27
Brianrahmarela <brian.rahmarela@gmail.com>
2021-04-06 01:04:35 +0700

    show login data at home pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,8 +17,8 @@ function Home() {
       <h3>Get data detail ukm</h3>
       {
          Object.entries(detailUkm).map(([item, value]) => (
-          console.log('ini item',item),
-          console.log('ini value',value),
+          // console.log('ini item',item),
+          // console.log('ini value',value),
           <div key={item}>
             <p>{value.name}</p>
             <p>{value.namaUkm}</p>
