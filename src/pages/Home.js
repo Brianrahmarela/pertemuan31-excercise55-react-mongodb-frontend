4bff336157113e9357d29359cd2bed98914b48cc
Brianrahmarela <brian.rahmarela@gmail.com>
2021-04-06 15:52:51 +0700

    show data user login at home pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,27 @@
-import {useEffect } from "react"
-import {useSelector, useDispatch} from 'react-redux';
-import {detailUkmAction} from '../redux/actions/home.action'
+import { useEffect } from "react"
+import { useSelector, useDispatch } from 'react-redux';
+import { detailUkmAction } from '../redux/actions/home.action'
 
 function Home() {
   const detailUkm = useSelector(state => state.detailUkm.data)
-  console.log('detailUkm dari store',detailUkm);
+  console.log('detailUkm dari store', detailUkm);
 
   const dispatch = useDispatch()
-  useEffect(()=> {
-      dispatch(detailUkmAction())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(detailUkmAction())
+  }, [dispatch])
 
   return (
+    <>
+    {(detailUkm.nama && detailUkm.ukm) &&
     <div>
-      <h1>Home</h1><br></br>
-      <h3>Get data detail ukm</h3>
-      {
-         Object.entries(detailUkm).map(([item, value]) => (
-          // console.log('ini item',item),
-          // console.log('ini value',value),
-          <div key={item}>
-            <p>{value.name}</p>
-            <p>{value.namaUkm}</p>
-            <p>{value.jenisUkm}</p>
-          </div>
-          ))   
-      }
+      <h1>{detailUkm.nama.name}</h1>
+      <h1>{detailUkm.ukm.jenisUkm}</h1>
+      <h1>{detailUkm.ukm.namaUkm}</h1>
+
     </div>
+    }
+    </>
   )
 }
 
